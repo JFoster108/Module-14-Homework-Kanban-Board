a844d5d392b0c5db1cb8408afb4b6b0afcb97f3e
JFoster108 <omario2005x@gmail.com>
2025-02-05 12:42:45 -0500

    revisions

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import authRoutes from './auth-routes.js';
+import userReports from '../models/user.js'
 import apiRoutes from './api/index.js';
 import { authenticateToken } from '../middleware/auth.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-// TODO: Add authentication to the API routes
+router.use("/users", authenticateToken, userRoutes);
 router.use('/api', authenticateToken, apiRoutes);
 
 export default router;
