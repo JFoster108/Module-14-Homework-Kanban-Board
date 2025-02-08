5ee060c3b815ad50c8f47665f5b0b4ccbc651451
JFoster108 <omario2005x@gmail.com>
2025-02-07 23:01:28 -0500

    fixed ports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,16 +3,16 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 5000,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
       '/auth': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false
       },
